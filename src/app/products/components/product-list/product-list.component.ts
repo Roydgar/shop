13b826655e8a6c4f9ed92d6083f2d9f0ab628e73
Viktor Gritsaenko <roydgaryshka@gmail.com>
2020-03-01 21:49:02 +0200

    Home Task 1

---
+++ src/app/products/components/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../model/product.model';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+
+  @Input()
+  products: Product[] ;
+
+  constructor() { }
+
+}
